c51a1f349f2f486d812befa471db42448c4bbfc2
sattrawoot_302004 <sttrawoot.p@kkumail.com>
2024-11-21 17:57:11 +0700

    1

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <nav className="bg-blue-500 py-4 fixed top-0 w-full z-50 ">
     <div className="container mx-auto flex justify-between items-center">
-      <a href="/" className="text-white font-bold text-xl">Resume</a>
+      <Link href="/" className="text-white font-bold text-xl">Resume</Link>
       <div className="flex space-x-4">
         <Link href="#Profile" className="text-white hover:bg-blue-600 px-2 py-1 rounded">Profile</Link>
         <Link href="#Education" className="text-white hover:bg-blue-600 px-2 py-1 rounded">Education</Link>
